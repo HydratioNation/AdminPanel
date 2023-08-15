7391f4b982695f4cf161b9683af574995edad252
VensanDrot <vensandrot@gmail.com>
2023-08-14 20:33:18 -0400

    Fixed layout

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../../../prisma";
+import { compare } from "bcrypt-ts";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      async authorize(credentials) {
+        const response = await prisma.admins.findFirst({
+          where: {
+            login: credentials?.login.toLowerCase(),
+          },
+        });
+
+        if (!response) {
+          throw new Error("No user was found");
+        }
+        const checkPass = await compare(credentials?.password, response?.password);
+        if (!checkPass || credentials?.login.toLowerCase() !== response.login.toLowerCase()) {
+          throw new Error("Email or password is wrong");
+        }
+        const user = {
+          _id: response.id,
+          email: response.id,
+          name: response.login,
+        };
+
+        return user;
+      },
+    }),
+  ],
+};
+export default NextAuth(authOptions);
